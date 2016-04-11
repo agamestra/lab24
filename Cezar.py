808d62c7688bc3d9f9577e44c2b2efdb22cc2c15
Valeriya <shyshygon@ya.ru>
2016-04-11 12:55:22 +0400

    Update Cezar.py

--- Cezar.py
+++ Cezar.py
@@ -6,7 +6,10 @@ class Caesar:
         uppercase_code = {self.alphabet[i].upper():self.alphabet[(i+key)%len(self.alphabet)].upper() for i in range(len(self.alphabet))}
         self._encode = dict(lowercase_code)
         self._encode.update(uppercase_code)
-        self._decode = {}  # FIXME
+        back_lowercase_code = {self.alphabet[(i+key)%len(self.alphabet)]:self.alphabet[i] for i in range(len(self.alphabet))}
+        back_uppercase_code = {self.alphabet[(i+key)%len(self.alphabet)].upper():self.alphabet[i].upper() for i in range(len(self.alphabet))}
+        self._decode = dict(back_lowercase_code)
+        self._decode.update(back_uppercase_code)
 
     def encode(self, line):
         if len(line) == 1:
@@ -15,12 +18,15 @@ class Caesar:
             return ''.join([self.encode(char) for char in line])
 
     def decode(self, line):
-        pass  # FIXME
+        if len(line) == 1:
+            return self._decode[line] if line in self._decode else line
+        else:
+            return ''.split([self.decode(char) for char in line])
 
 
 key = int(input('Введите ключ:'))
 cipher = Caesar(key)
 line = input()
-while line:
+while line != '.':
     print(cipher.encode(line))
     line = input()
