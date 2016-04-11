ea9f07643af537925f28442a4fcf55bdf8b78959
Valeriya <shyshygon@ya.ru>
2016-04-11 11:12:25 +0300

    Added files via upload

---
+++ Cezar.py
@@ -0,0 +1,26 @@
+class Caesar:
+    alphabet = "яюэьыъщшчцхфутсрпонмлкйизжёедгвба"
+
+    def __init__(self, key):
+        lowercase_code = {self.alphabet[i]:self.alphabet[(i+key)%len(self.alphabet)] for i in range(len(self.alphabet))}
+        uppercase_code = {self.alphabet[i].upper():self.alphabet[(i+key)%len(self.alphabet)].upper() for i in range(len(self.alphabet))}
+        self._encode = dict(lowercase_code)
+        self._encode.update(uppercase_code)
+        self._decode = {}  # FIXME
+
+    def encode(self, line):
+        if len(line) == 1:
+            return self._encode[line] if line in self._encode else line
+        else:
+            return ''.join([self.encode(char) for char in line])
+
+    def decode(self, line):
+        pass  # FIXME
+
+
+key = int(input('Введите ключ:'))
+cipher = Caesar(key)
+line = input()
+while line:
+    print(cipher.encode(line))
+    line = input()
